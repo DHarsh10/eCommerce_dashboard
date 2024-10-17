01cf5ecda60cf93ca2ccaff252fa319abed72025
DHarsh10 <92229629+DHarsh10@users.noreply.github.com>
2024-10-17 17:33:35 +0530

    Update dafaultData.js

--- src/data/dafaultData.js
+++ src/data/dafaultData.js
@@ -8,25 +8,27 @@ import {
 export const ordersData = [
     {
         name: "Customers",
-        numbers: "3,781",
+        numbers: 3781,
         stonks: "+11.01%",
         img: [stonksUp, stonksUp]
     },
     {
         name: "Orders",
-        numbers: "1,219",
+        numbers: 1219,
         stonks: "-0.03%",
         img: [stonksDown, stonksDownDark]
     },
     {
         name: "Revenue",
-        numbers: "$695",
+        numbers: 695,
+        unitStart: "$",
         stonks: "+15.03%",
         img: [stonksUp, stonksUpDark]
     },
     {
         name: "Growth",
-        numbers: "30.1%",
+        numbers: 30.1,
+        unitEnd: "%",
         stonks: "+6.08%",
         img: [stonksUp, stonksUp]
     }
